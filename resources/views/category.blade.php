bf67ff4f6378bfe1e2707fae2fa04c6d81d30a65
roman <s210240@gmail.com>
2019-09-26 17:45:51 +0300

    update views

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
     <div class="container">
+        <p>News from category: </p>
         @foreach($news as $item)
             <div class="card" style="width: 90%;">
                 <div class="card-body">
                     <h5 class="card-title"><a href="/news/{{$item->id}}">{{$item->header}}</a></h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><a href="category/{{$item->category_id}}">{{$item->category->header}}</a></h6>
+                    <h6 class="card-subtitle mb-2 text-muted">{{$item->category->header}}</h6>
                     <p class="card-text">{{ Str::limit($item->content, 10, '...') }}</p>
                 </div>
             </div>
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="alignment: center">
-        <h4>
-            {{$news->header}}
-        </h4>
-        <p>{{$news->content}}</p>
+    <div class="container">
+        <div class="card" style="width: 80%;">
+            <div class="card-body">
+                <h5 class="card-title">{{$news->header}}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{$news->category->header}}</h6>
+                <p class="card-text">{{ $news->content}}</p>
+                @guest
+                    <a href="#" class="card-link">Subscribe this news category</a>
+                @else
+                    <a href="#" class="card-link">Unsubscribe this news category</a>
+                @endguest
+            </div>
+        </div>
     </div>
 @endsection
