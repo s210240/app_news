2fdf815fce98f385b032f0345e0fbe582ae37533
roman <s210240@gmail.com>
2019-09-26 15:58:32 +0300

    new routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MainController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/category', 'CategoryController@index');
+
+Route::get('/news/{id}', 'MainController@show');
