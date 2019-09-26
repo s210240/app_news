a7f8270610b012d7c0a221e57eba926ad41b3be9
roman <s210240@gmail.com>
2019-09-26 09:13:07 +0300

    INIT REPO

---
+++ database/migrations/2019_09_26_060329_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('header');
+            $table->string('content');
+            $table->integer('id_category')->unsigned()->index();
+            $table->timestamps();
+
+            $table
+                ->foreign('id_category')
+                ->references('id')
+                ->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
