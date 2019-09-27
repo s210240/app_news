fa23ac3b08987fefc2b23915aac1781d711707a3
roman <s210240@gmail.com>
2019-09-27 07:42:28 +0300

    Vue subscribe

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button v-on:click="subNews()">Subscribe</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PostComponent",
+
+        props: ['surveyData'],
+
+        mounted () {
+            console.dir(this.surveyData)
+        },
+
+        methods: {
+            subNews() {
+                let self = this;
+                let id = self.surveyData;
+                if (confirm("Do you really want to subscribe it?")) {
+                    axios.post('/subscribe', {
+                        id: id
+                    })
+                        .then(function (response) {
+                            console.log(response);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
