50b612f713fee86b54d7b06cb7834eefcf514e38
roman <s210240@gmail.com>
2019-09-27 09:27:44 +0300

    add simple search

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Item;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class SearchController extends Controller
+{
+    public function index(Request $request)
+    {
+        $searchString = $request->searchString;
+
+        $data = Item::with('category')->where('header', 'LIKE', '%' . $searchString . '%')->paginate(10);
+
+        return view('welcome', ['news' => $data]);
+    }
+}
